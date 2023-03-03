65c134de6297c9507f449e28a11f6c0687a3502f
juan lopez <jflopez93@hotmail.com>
2023-03-02 20:59:05 -0500

    segundo fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -81,7 +81,7 @@ return (
                     leaveTo="transform opacity-0 scale-95"
                     >
                     <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <button onClick={() => auth.logout()} className= 'block px-4 py-2 text-sm text-gray-700'>
+                            <button onClick={() => auth.logOut()} className= 'block px-4 py-2 text-sm text-gray-700'>
                                 Logout
                             </button>
                     </Menu.Items>
